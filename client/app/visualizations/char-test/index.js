4da0d4b77d38c5eff88d67ca779bb601255ea0c9
rpdict <rpdict@gmail.com>
2018-05-07 13:31:33 +0800

    add echarts pie visualization

--- client/app/visualizations/char-test/index.js
+++ client/app/visualizations/char-test/index.js
@@ -1,8 +1,3 @@
-// import {
-//   some, extend, has, partial, intersection, without, contains, isUndefined,
-//   sortBy, each, pluck, keys, difference,
-// } from 'underscore';
-import angular from 'angular';
 import charTemplate from './char-test.html';
 import charEditorTemplate from './char-test-editor.html';
 
@@ -15,29 +10,28 @@ function charTest() {
         const queryData = $scope.queryResult.getData();
         if (queryData) {
           // do the render logic.
-          const xAxisData = $scope.visualization.options.xColumn;
-          const seriesData = $scope.visualization.options.yColumns;
-          const seriesName = $scope.visualization.options.chart.name;
-          const xAxisDataArray = [];
-          const seriesDataArray = [];
-          angular.forEach(queryData, (value) => {
-            xAxisDataArray.push(value[xAxisData]);
-            seriesDataArray.push(value[seriesData]);
-          });
-          const series = {
-            type: $scope.visualization.options.chart.type,
-            name: seriesName,
-            data: seriesDataArray,
-          };
-          console.log(seriesDataArray);
+          $scope.chartOption = $scope.visualization.options;
 
-          $scope.barOption = $scope.visualization.options;
-          $scope.barOption.title.text = $scope.visualization.name;
-          $scope.barOption.legend.data = [seriesName];
-          $scope.barOption.series = [series];
-          $scope.barOption.xAxis = $scope.visualization.options.xAxis;
-          $scope.barOption.xAxis.data = xAxisDataArray;
-          console.log($scope.barOption);
+          $scope.chartOption.dataset.dimensions = [
+            $scope.visualization.options.xColumn,
+            $scope.visualization.options.yColumns,
+          ];
+          $scope.chartOption.dataset.source = queryData;
+          $scope.chartOption.series = [
+            {
+              type: $scope.visualization.options.chart.type,
+              name: $scope.visualization.options.chart.name,
+            },
+          ];
+          if ($scope.visualization.options.chart.type === 'pie') {
+            $scope.chartOption.xAxis.show = false;
+            $scope.chartOption.yAxis.show = false;
+          } else {
+            $scope.chartOption.xAxis.show = true;
+            $scope.chartOption.yAxis.show = true;
+          }
+
+          console.log($scope.chartOption);
         }
       };
 
@@ -52,6 +46,12 @@ function charTestEditor() {
     restrict: 'E',
     template: charEditorTemplate,
     link($scope) {
+      $scope.currentTab = 'general';
+
+      $scope.changeTab = (tab) => {
+        $scope.currentTab = tab;
+      };
+
       const refreshData = () => {
         const queryData = $scope.queryResult.getData();
         if (queryData) {
@@ -59,11 +59,11 @@ function charTestEditor() {
           $scope.chartTypes = {
             line: { name: 'Line', icon: 'line-chart' },
             bar: { name: 'Bar', icon: 'bar-chart' },
-            area: { name: 'Area', icon: 'area-chart' },
+            // area: { name: 'Area', icon: 'area-chart' },
             pie: { name: 'Pie', icon: 'pie-chart' },
-            scatter: { name: 'Scatter', icon: 'circle-o' },
-            bubble: { name: 'Bubble', icon: 'circle-o' },
-            box: { name: 'Box', icon: 'square-o' },
+            // scatter: { name: 'Scatter', icon: 'circle-o' },
+            // bubble: { name: 'Bubble', icon: 'circle-o' },
+            // box: { name: 'Box', icon: 'square-o' },
           };
         }
       };
@@ -82,7 +82,9 @@ export default function init(ngModule) {
     title: {},
     legend: {},
     tooltip: {},
+    dataset: {},
     xAxis: {
+      type: 'category',
       axisLabel: {
         interval: 0, // 横轴信息全部显示
         rotate: -30, // -30度角倾斜显示
